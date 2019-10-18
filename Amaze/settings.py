4fa6422b9201c9b886f0679001b5134600bcc496
Maxoffline <cbv.python@gmail.com>
2019-10-18 17:42:21 -0400

    Changed the root to "staticfiles/" or it won't collect static.

--- Amaze/settings.py
+++ Amaze/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Location of static files in production enviroment.
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 """ "STATICFILES_DIR" is the location of which Django will grab it's files 
 # then move it to the "STATIC_ROOT"  """
