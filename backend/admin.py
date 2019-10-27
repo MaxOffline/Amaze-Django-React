fc3814fa43bed4ccd9894856fca07b40d9f2e728
Maxoffline <cbv.python@gmail.com>
2019-10-26 22:46:14 -0400

    Added test Model

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from backend.models import Products, Cart, CartProduct
+from backend.models import Products, Cart, CartProduct,TestModel
 
 admin.site.register(Cart)
 admin.site.register(CartProduct)
 admin.site.register(Products)
+admin.site.register(TestModel)
+
--- backend/models.py
+++ backend/models.py
@@ -19,7 +19,10 @@ class Products (models.Model):
     
     def __str__(self):
         return self.title
-
+class TestModel (models.Model):
+    bomba = models.IntegerField(blank = False)
+    def __str__(self):
+        return str(self.bomba)
 
 
 
