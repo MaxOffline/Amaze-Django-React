14e82c95e58b08e106965565bbd7a23cbc3250d7
Maxoffline <cbv.python@gmail.com>
2019-10-31 20:55:08 -0400

    Commented out static storage

--- Amaze/settings.py
+++ Amaze/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/build/static")]
 CORS_ORIGIN_ALLOW_ALL = True
 # Maybe used if having CSRF issues/
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Activate Django-Heroku.
