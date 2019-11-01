0503b7f0e23d2cd79a5d865b1e24a4bdc203e74d
Maxoffline <cbv.python@gmail.com>
2019-10-31 20:23:50 -0400

    Trying postgres DB

--- Amaze/settings.py
+++ Amaze/settings.py
@@ -1,5 +1,10 @@
 import os
 import django_heroku
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 """Build paths inside the project like this: os.path.join(BASE_DIR, ...)"""
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +151,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
