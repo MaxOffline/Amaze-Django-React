4ede645bba8c5ad1ab0d4f95e7e1db321511a4b8
Maxoffline <cbv.python@gmail.com>
2019-11-24 02:03:16 -0500

    Removed whitenoise middleware

--- Amaze/settings.py
+++ Amaze/settings.py
@@ -68,15 +68,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # added to solve CORS
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Added to redirect from HTTP to HTTPS
